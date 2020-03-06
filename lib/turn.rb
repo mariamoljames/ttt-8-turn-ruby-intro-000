4992ca506e28962ed52a6846921c1f625d09b189
lite-helper-5188 <mariamol.james@gmail.com>
2020-03-06 16:40:46 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -10,6 +10,23 @@ def input_to_index(input)
   index=input.to_i - 1
 end
 
-def move(board, index, value='X')
+def position_taken?(board,index)
+  if board[index]==" "||board[index]==""||board[index]==nil
+    false
+  else
+    true
+  end
+end
+
+def valid_move?(board,index)
+  if index.between?(0,8) && !(position_taken?(board,index))
+    true
+  else
+    false
+  end
+end
+
+def move(board, index, value=)
   board[index] = value
 end
+
